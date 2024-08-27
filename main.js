b035ce64f29d3eccee88f59e472b17c2291452c4
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2024-08-27 19:45:12 +0300

    get-total

---
+++ main.js
@@ -0,0 +1,21 @@
+let title=document.getElementById('title');
+let price=document.getElementById('price');
+let taxes=document.getElementById('taxes');
+let ads=document.getElementById('ads');
+let discount=document.getElementById('discount');
+let total=document.getElementById('total');
+let count=document.getElementById('count');
+let category=document.getElementById('category');
+let submit=document.getElementById('submit');
+//get total
+function getTotal(){
+if(price.value !=''){
+    let result=(+price.value+ +taxes.value + +ads.value)- +discount.value;
+    total.innerHTML=result;
+    total.style.background='#040';
+}
+else{
+    total.innerHTML='';
+    total.style.background='#a00d';
+}
+}
